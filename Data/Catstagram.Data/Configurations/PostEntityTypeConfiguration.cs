6fdef341949ad3ecc9cabb2c3563544aab79d8fe
Banovvv <ivan_banov@abv.bg>
2022-10-13 18:28:54 +0300

    empty EntityTypeConfiguration

---
+++ Data/Catstagram.Data/Configurations/PostEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catstagram.Data.Configurations
+{
+    internal class PostEntityTypeConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Catstagram.Data/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catstagram.Data.Configurations
+{
+    internal class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
