48469954bb52b45724a483d0744609a569f8192d
Banovvv <ivan_banov@abv.bg>
2022-10-13 18:26:37 +0300

    CatsDataContext

---
+++ Data/Catstagram.Data/CatsDataContext.cs
@@ -0,0 +1,33 @@
+﻿using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catstagram.Data
+{
+    public class CatsDataContext : DbContext
+    {
+        public CatsDataContext()
+        {
+        }
+
+        public CatsDataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Data/Catstagram.Data/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace Catstagram.Data
+{
+    internal class Configuration
+    {
+        internal static string GetConnectionString()
+        {
+            return @"Server=.\SQLEXPRESS;Database=Catstagram;Integrated Security=true;";
+        }
+    }
+}
