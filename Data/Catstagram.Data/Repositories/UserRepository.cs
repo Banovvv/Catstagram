b700edcef7fffb9776e9f189a32bdf52506cf992
Banovvv <ivan_banov@abv.bg>
2022-10-14 18:47:51 +0300

    : Repository<Post> in User and Post repositories

--- Data/Catstagram.Data/Repositories/UserRepository.cs
+++ Data/Catstagram.Data/Repositories/UserRepository.cs
@@ -1,8 +1,12 @@
 ﻿using Catstagram.Data.Common.Repositories;
+using Catstagram.Data.Models;
 
 namespace Catstagram.Data.Repositories
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : Repository<User>, IUserRepository
     {
+        public UserRepository(CatsDataContext context) : base(context)
+        {
+        }
     }
 }
