ca412666125f1db9e60bb787ff610933e838d06d
Ivan Gechev <ivan_banov@abv.bg>
2022-10-14 19:08:12 +0300

    Basic UserRepository

--- Data/Catstagram.Data.Common/Repositories/IUserRepository.cs
+++ Data/Catstagram.Data.Common/Repositories/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Catstagram.Data.Common.Repositories
+﻿using Catstagram.Data.Models;
+
+namespace Catstagram.Data.Common.Repositories
 {
     public interface IUserRepository
     {
+        int GetCount();
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<IEnumerable<User>> GetByIdAsync(int id);
+        Task<IEnumerable<User>> GetByUsernameAsync(string username);
     }
 }
--- Data/Catstagram.Data/Repositories/UserRepository.cs
+++ Data/Catstagram.Data/Repositories/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Catstagram.Data.Common.Repositories;
 using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catstagram.Data.Repositories
 {
@@ -7,6 +8,31 @@ namespace Catstagram.Data.Repositories
     {
         public UserRepository(CatsDataContext context) : base(context)
         {
+        }
+
+        public int GetCount()
+        {
+            return this.AllAsNoTracking().Count();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await this.DbSet
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetByIdAsync(int id)
+        {
+            return await this.DbSet
+                .Where(x => x.Id == id)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> GetByUsernameAsync(string username)
+        {
+            return await this.DbSet
+                .Where(x => x.Username == username)
+                .ToListAsync();
         }
     }
 }
