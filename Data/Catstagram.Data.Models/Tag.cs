3b6b954ea1935805287b499a1c389978de79e8c1
Banovvv <ivan_banov@abv.bg>
2022-10-16 08:31:56 +0300

    Tag, Posts with tag, Tags in Posts

---
+++ Data/Catstagram.Data.Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿namespace Catstagram.Data.Models
+{
+    public class Tag
+    {
+        public Tag()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
