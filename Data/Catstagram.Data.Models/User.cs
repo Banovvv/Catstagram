7d35beba71c1595acdbb793cb6340f4a653e5769
Banovvv <ivan_banov@abv.bg>
2022-10-17 12:05:48 +0300

    Comment model and relationships

--- Data/Catstagram.Data.Models/User.cs
+++ Data/Catstagram.Data.Models/User.cs
@@ -6,6 +6,7 @@
         {
             Posts = new HashSet<Post>();
             Likes = new HashSet<Like>();
+            Comments = new HashSet<Comment>();
         }
 
         public int Id { get; set; }
@@ -19,5 +20,6 @@
 
         public virtual ICollection<Post> Posts { get; set; }
         public virtual ICollection<Like> Likes { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
