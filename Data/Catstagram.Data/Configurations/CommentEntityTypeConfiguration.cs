8c1c0b8bf38d5bf93560fbde9cfa66edb4283c5a
Banovvv <ivan_banov@abv.bg>
2022-10-17 12:19:49 +0300

    CommentEntityTypeConfiguration

---
+++ Data/Catstagram.Data/Configurations/CommentEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catstagram.Data.Configurations
+{
+    internal class CommentEntityTypeConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> comment)
+        {
+            comment.Property(x => x.Text)
+                .IsRequired(true)
+                .HasMaxLength(250)
+                .IsUnicode(true);
+
+            comment.Property(x => x.CreatedOn)
+                .HasDefaultValueSql("GETDATE()")
+                .IsRequired(true);
+
+            comment.Property(x => x.UserId)
+                .IsRequired(true);
+
+            comment.Property(x => x.PostId)
+                .IsRequired(true);
+        }
+    }
+}
