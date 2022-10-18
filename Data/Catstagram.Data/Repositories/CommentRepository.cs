de4dff91c790346c429cf0fcfa9a2a3d0a39293a
Banovvv <ivan_banov@abv.bg>
2022-10-18 13:24:08 +0300

    CommentRepository

---
+++ Data/Catstagram.Data/Repositories/CommentRepository.cs
@@ -0,0 +1,47 @@
+﻿using Catstagram.Data.Common.Repositories;
+using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catstagram.Data.Repositories
+{
+    public class CommentRepository : Repository<Comment>, ICommentRepository
+    {
+        public CommentRepository(CatsDataContext context) : base(context)
+        {
+        }
+
+        public int GetCount()
+        {
+            return this.AllAsNoTracking().Count();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await this.DbSet
+                .Where(x => x.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllByPostIdAsync(int id)
+        {
+            return await this.DbSet
+                .Where(x => x.PostId == id)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllByUserIdAsync(int id)
+        {
+            return await this.DbSet
+                .Where(x => x.UserId == id)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetAllByUsernameAsync(string username)
+        {
+            return await this.DbSet
+                .Include(x => x.User)
+                .Where(x => x.User.Username == username)
+                .ToListAsync();
+        }
+    }
+}
