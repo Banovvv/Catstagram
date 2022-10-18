69d1ab3c8b0ca90664d9b1d8f957d0e79aa3f0b4
Banovvv <ivan_banov@abv.bg>
2022-10-18 11:41:05 +0300

    ICommentRepository

---
+++ Data/Catstagram.Data.Common/Repositories/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catstagram.Data.Models;
+
+namespace Catstagram.Data.Common.Repositories
+{
+    public interface ICommentRepository
+    {
+        int GetCount();
+        Task<Comment?> GetByIdAsync(int id);
+        Task<IEnumerable<Comment>> GetAllByUsernameAsync(string username);
+        Task<IEnumerable<Comment>> GetAllByUserIdAsync(int id);
+        Task<IEnumerable<Comment>> GetAllByPostIdAsync(int id);
+    }
+}
