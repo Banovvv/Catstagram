99deeb3db8df7924b4240efe7cd8e68c6cc38b80
Banovvv <ivan_banov@abv.bg>
2022-10-18 08:15:33 +0300

    ITagRepository and TagRepository

---
+++ Data/Catstagram.Data.Common/Repositories/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using Catstagram.Data.Models;
+
+namespace Catstagram.Data.Common.Repositories
+{
+    public interface ITagRepository
+    {
+        int GetCount();
+        Task<Tag?> GetByIdAsync(int id);
+        Task<Tag?> GetByNameAsync(string name);
+        Task<IEnumerable<Tag>> GetAllAsync();
+    }
+}
---
+++ Data/Catstagram.Data/Repositories/TagRepository.cs
@@ -0,0 +1,39 @@
+﻿using Catstagram.Data.Common.Repositories;
+using Catstagram.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catstagram.Data.Repositories
+{
+    public class TagRepository : Repository<Tag>, ITagRepository
+    {
+        public TagRepository(CatsDataContext context) : base(context)
+        {
+        }
+
+        public int GetCount()
+        {
+            return this.AllAsNoTracking().Count();
+        }
+
+
+        public async Task<Tag?> GetByIdAsync(int id)
+        {
+            return await this.DbSet
+                .Where(x => x.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<Tag?> GetByNameAsync(string name)
+        {
+            return await this.DbSet
+                .Where(x => x.Name == name)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            return await this.DbSet
+                .ToListAsync();
+        }
+    }
+}
