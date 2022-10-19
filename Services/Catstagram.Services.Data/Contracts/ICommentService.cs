534350dda4c48bddde585f3e17431e1baf5b483b
Banovvv <ivan_banov@abv.bg>
2022-10-19 16:02:41 +0300

    ICommentService

---
+++ Services/Catstagram.Services.Data/Contracts/ICommentService.cs
@@ -0,0 +1,11 @@
+﻿using Catstagram.Data.Models;
+
+namespace Catstagram.Services.Data.Contracts
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<Comment>> GetALLByPostAsync(int id);
+        Task<IEnumerable<Comment>> GetALLByUserAsync(int id);
+        Task<IEnumerable<Comment>> GetALLByUserAsync(string username);
+    }
+}
