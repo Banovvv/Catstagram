27f3e870e25c4baa9d98506ec9834514d6ebb559
Banovvv <ivan_banov@abv.bg>
2022-10-20 12:15:06 +0300

    TagService and ITagService

---
+++ Services/Catstagram.Services.Data/Contracts/ITagService.cs
@@ -0,0 +1,11 @@
+﻿using Catstagram.Data.Models;
+
+namespace Catstagram.Services.Data.Contracts
+{
+    public interface ITagService
+    {
+        Task<Tag?> GetByIdAsync(int id);
+        Task<Tag?> GetByNameAsync(string name);
+        Task<IEnumerable<Tag>> GetAllAsync();
+    }
+}
---
+++ Services/Catstagram.Services.Data/TagService.cs
@@ -0,0 +1,31 @@
+﻿using Catstagram.Data.Common.Repositories;
+using Catstagram.Data.Models;
+using Catstagram.Services.Data.Contracts;
+
+namespace Catstagram.Services.Data
+{
+    public class TagService : ITagService
+    {
+        private readonly ITagRepository repository;
+
+        public TagService(ITagRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            return await this.repository.GetAllAsync();
+        }
+
+        public async Task<Tag?> GetByIdAsync(int id)
+        {
+            return await this.repository.GetByIdAsync(id);
+        }
+
+        public async Task<Tag?> GetByNameAsync(string name)
+        {
+            return await this.repository.GetByNameAsync(name);
+        }
+    }
+}
