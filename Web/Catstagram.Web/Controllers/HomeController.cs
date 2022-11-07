d5a4ede2428b4cfbcad87648f99ebfadb9b2933d
Ivan Gechev <ivan_banov@abv.bg>
2022-11-07 17:38:58 +0200

    ErrorViewModel

--- Web/Catstagram.Web/Controllers/HomeController.cs
+++ Web/Catstagram.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Catstagram.Services.Data.Contracts;
+using Catstagram.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Catstagram.Web.Controllers
 {
@@ -17,6 +19,13 @@ namespace Catstagram.Web.Controllers
             var model = await _postService.GetAllAsync();
 
             return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+                { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Web/Catstagram.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Catstagram.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
