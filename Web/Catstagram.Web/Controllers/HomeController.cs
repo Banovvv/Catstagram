4dedcafc449a582cddf1f59a3d0d12640c4295b1
Ivan Gechev <ivan_banov@abv.bg>
2022-11-07 16:05:20 +0200

    Working on getting post on home page

--- Web/Catstagram.Web/Controllers/HomeController.cs
+++ Web/Catstagram.Web/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catstagram.Services.Data.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Catstagram.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IPostService _postService;
+
+        public HomeController(IPostService postService)
         {
-            return View();
+            _postService = postService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var model = await _postService.GetAllAsync();
+
+            return View(model);
         }
     }
 }
