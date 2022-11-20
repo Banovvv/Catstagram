e48bdb3a6321db2845e3ad6f3565d712c45b7f28
Ivan Gechev <ivan_banov@abv.bg>
2022-11-20 10:12:30 +0200

    Basic tests files

---
+++ Tests/Catstagram.Web.API.Tests/Repositories/PostRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catstagram.Web.API.Tests.Repositories
+{
+    internal class PostRepositoryTests
+    {
+    }
+}
