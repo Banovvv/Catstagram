ad970b62ef84b5fc2f1ff301bb58eff75d312b1e
Ivan Gechev <ivan_banov@abv.bg>
2022-11-20 10:08:24 +0200

    API controller work

--- Web/Catstagram.Web.API/Controllers/PostController.cs
+++ Web/Catstagram.Web.API/Controllers/PostController.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catstagram.Data.Common.Repositories;
+using Catstagram.Data.Models;
+using Catstagram.Services.Data.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Catstagram.Web.API.Controllers
 {
     [ApiController]
     public class PostController : Controller
     {
-        
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<IEnumerable<Post>>> GetAllAsync()
+        {
+            var posts = await _postService.GetAllAsync();
+
+            return Ok(posts);
+        }
+
+        [HttpGet("GetTopTen")]
+        public async Task<ActionResult<IEnumerable<Post>>> GetTopTenAsync()
+        {
+            var topTen = await _postService.GetTopTenAsync();
+
+            return Ok(topTen);
+        }
     }
 }
