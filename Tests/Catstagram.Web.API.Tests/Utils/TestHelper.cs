9b21bb349c1d211ceec47bcbd91be44b09be30b9
Ivan Gechev <ivan_banov@abv.bg>
2022-11-20 10:14:59 +0200

    TestHelper

---
+++ Tests/Catstagram.Web.API.Tests/Utils/TestHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catstagram.Web.API.Tests.Utils
+{
+    public class TestHelper
+    {
+        public static T GetObjectResultContent<T>(ActionResult<T> result)
+        {
+            return (T)((ObjectResult)result.Result).Value;
+        }
+    }
+}
