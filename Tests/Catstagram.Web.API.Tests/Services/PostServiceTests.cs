e15627dcd33dff875c39b134ec9cabd63287338b
Ivan Gechev <ivan_banov@abv.bg>
2022-11-23 09:24:01 +0200

    setting up PostServiceTests

--- Tests/Catstagram.Web.API.Tests/Services/PostServiceTests.cs
+++ Tests/Catstagram.Web.API.Tests/Services/PostServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Catstagram.Data.Common.Repositories;
+using Catstagram.Services.Data;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,24 @@ using System.Threading.Tasks;
 
 namespace Catstagram.Web.API.Tests.Services
 {
-    internal class PostServiceTests
+    public class PostServiceTests
     {
+        private IFixture _fixture;
+        private readonly PostService _postService;
+        private readonly Mock<IPostRepository> _postRepository;
+
+        public PostServiceTests()
+        {
+            _postRepository = new Mock<IPostRepository>();
+            _postService = new PostService(_postRepository.Object);
+        }
+
+        private void SetupFixture()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => _fixture.Behaviors.Remove(b));
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
     }
 }
