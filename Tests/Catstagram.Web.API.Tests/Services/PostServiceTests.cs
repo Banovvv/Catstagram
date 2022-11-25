0ae804a88f3c186c414b39f858d26ecfa3cad805
Ivan Gechev <ivan_banov@abv.bg>
2022-11-25 08:32:27 +0200

    Moved SetupAutoFixture() to TestHelper class

--- Tests/Catstagram.Web.API.Tests/Services/PostServiceTests.cs
+++ Tests/Catstagram.Web.API.Tests/Services/PostServiceTests.cs
@@ -4,6 +4,7 @@ using Catstagram.Data.Common.Exceptions.Models;
 using Catstagram.Data.Common.Repositories;
 using Catstagram.Data.Models;
 using Catstagram.Services.Data;
+using Catstagram.Web.API.Tests.Utils;
 using Moq;
 
 namespace Catstagram.Web.API.Tests.Services
@@ -16,7 +17,7 @@ namespace Catstagram.Web.API.Tests.Services
 
         public PostServiceTests()
         {
-            _fixture = SetupAutoFixture();
+            _fixture = TestHelper.SetupAutoFixture();
 
             _postRepository = _fixture.Freeze<Mock<IPostRepository>>();
             _postService = new PostService(_postRepository.Object);
@@ -125,17 +126,6 @@ namespace Catstagram.Web.API.Tests.Services
 
             //Assert
             Assert.Equal(10, result.Count());
-        }
-
-        private static IFixture SetupAutoFixture()
-        {
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            fixture.Behaviors
-                .OfType<ThrowingRecursionBehavior>().ToList()
-                .ForEach(b => fixture.Behaviors.Remove(b));
-            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
-
-            return fixture;
         }
     }
 }
--- Tests/Catstagram.Web.API.Tests/Utils/TestHelper.cs
+++ Tests/Catstagram.Web.API.Tests/Utils/TestHelper.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Catstagram.Web.API.Tests.Utils
 {
@@ -7,6 +9,17 @@ namespace Catstagram.Web.API.Tests.Utils
         public static T GetObjectResultContent<T>(ActionResult<T> result)
         {
             return (T)((ObjectResult)result.Result).Value;
+        }
+
+        public static IFixture SetupAutoFixture()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            fixture.Behaviors
+                .OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            return fixture;
         }
     }
 }
