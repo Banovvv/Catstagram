b367174579cf0f9252a206e5af2d25542eae681f
Ivan Gechev <ivan_banov@abv.bg>
2022-11-25 16:18:17 +0200

    PostControllerTests setup

--- Tests/Catstagram.Web.API.Tests/Controllers/PostControllerTests.cs
+++ Tests/Catstagram.Web.API.Tests/Controllers/PostControllerTests.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoFixture;
+using AutoMapper;
+using Catstagram.Services.Data.Contracts;
+using Catstagram.Web.API.Controllers;
+using Catstagram.Web.API.Tests.Utils;
+using Moq;
 
 namespace Catstagram.Web.API.Tests.Controllers
 {
-    internal class PostControllerTests
+    public class PostControllerTests
     {
+        private IFixture _fixture;
+        private readonly Mock<IMapper> _mapper;
+        private readonly PostController _postController;
+        private readonly Mock<IPostService> _postService;
+
+        public PostControllerTests()
+        {
+            _fixture = TestHelper.SetupAutoFixture();
+            _mapper = _fixture.Freeze<Mock<IMapper>>();
+            _postService = _fixture.Freeze<Mock<IPostService>>();
+            _postController = new PostController(_postService.Object, _mapper.Object);
+        }
     }
 }
