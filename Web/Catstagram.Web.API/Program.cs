32036a06c364fc5e34a472602b0ba9bbc94d6f40
Ivan Gechev <ivan_banov@abv.bg>
2023-03-13 10:58:15 +0200

    restructuring

--- Web/Catstagram.Web.API/Program.cs
+++ Web/Catstagram.Web.API/Program.cs
@@ -6,47 +6,50 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var configuration = builder.Configuration;
-configuration.AddJsonFile("appsettings.json", false);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo
+
+    var configuration = builder.Configuration;
+    configuration.AddJsonFile("appsettings.json", false);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
     {
-        Title = "Catstagram_API",
-        Version = "v1"
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Catstagram_API",
+            Version = "v1"
+        });
     });
-});
 
-builder.Services
-    .AddDbContext<CatsDataContext>(options =>
-        options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));
+    builder.Services
+        .AddDbContext<CatsDataContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString")));
 
-builder.Services.AddCatstagramServices();
-builder.Services.AddCatstagramRepositories();
+    builder.Services.AddCatstagramServices();
+    builder.Services.AddCatstagramRepositories();
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddValidatorsFromAssemblyContaining<PostValidator>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddValidatorsFromAssemblyContaining<PostValidator>();
 }
 
-app.UseHttpsRedirection();
+var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseAuthorization();
+    app.UseHttpsRedirection();
 
-app.MapControllers();
+    app.UseAuthorization();
 
-app.Run();
+    app.MapControllers();
+
+    app.Run();
+}
